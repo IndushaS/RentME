8395f42a75a2a7795ad39f8d0d06f8bbcbbc3ce2
Indusha <indusha@Indushas-MacBook-Air.local>
2020-03-13 13:45:13 -0400

    setup

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,11 +1,8 @@
 import time
-from flask import Flask 
+from flask import Flask
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/time')
-def get_current_time();
-	return{'time':time.time()}
-
-
-
+def get_current_time():
+    return {'time': time.time()}
